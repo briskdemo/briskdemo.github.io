6d4462fd2c8d4bdf21ed5424c1923c42a9752f09
Sunaif_adkar <80641581+sunaif-adkar2@users.noreply.github.com>
2023-11-05 13:05:31 +0530

    Create script.js

---
+++ script.js
@@ -0,0 +1,15 @@
+document.getElementById("button1").addEventListener("click", function() {
+    window.location.href = "https://wa.me/your-whatsapp-link";
+});
+
+document.getElementById("button2").addEventListener("click", function() {
+    window.location.href = "https://wa.me/your-whatsapp-link";
+});
+
+document.getElementById("button3").addEventListener("click", function() {
+    window.location.href = "https://wa.me/your-whatsapp-link";
+});
+
+document.getElementById("button4").addEventListener("click", function() {
+    window.location.href = "https://wa.me/your-whatsapp-link";
+});
