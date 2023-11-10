3c4754c13de0c2fe99ee18847ee5660074f244ca
Sunaif_adkar <80641581+sunaif-adkar2@users.noreply.github.com>
2023-11-10 19:56:26 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
-$(document).ready(function () {
-    $(".contact-button").click(function () {
-        window.open("https://twitter.com/sunaifn", "_blank");
-    });
+const contactButton = document.querySelector('.contact-button');
+
+contactButton.addEventListener('click', () => {
+  window.open('https://twitter.com/sunaifn');
 });
