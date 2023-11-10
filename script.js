1694d37c52b14750321d58e14426df7c26280f65
Sunaif_adkar <80641581+sunaif-adkar2@users.noreply.github.com>
2023-11-10 18:30:37 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,15 +1,5 @@
-document.getElementById("button1").addEventListener("click", function() {
-    window.location.href = "https://wa.me/+917483862447";
-});
-
-document.getElementById("button2").addEventListener("click", function() {
-    window.location.href = "https://wa.me/+917483862447";
-});
-
-document.getElementById("button3").addEventListener("click", function() {
-    window.location.href = "https://wa.me/+917483862447";
-});
-
-document.getElementById("button4").addEventListener("click", function() {
-    window.location.href = "https://wa.me/+917483862447";
+$(document).ready(function () {
+    $(".contact-button").click(function () {
+        window.open("https://twitter.com/sunaifn", "_blank");
+    });
 });
